b749bc697b87bee3ec34f6f459e6b297f1cc2fe4
John Carlo Ancheta <uif60764@automotive-wan.com>
2026-02-11 13:18:32 +0800

    update 11_3

---
+++ PasswordPopup.h
@@ -0,0 +1,33 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/* Copyright (c) National Instruments 2026. All Rights Reserved.          */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  PANEL_PW                         1
+#define  PANEL_PW_PANEL_PASSWORD_STRING   2
+#define  PANEL_PW_PANEL_PASSWORD_OK_BTN   3
+#define  PANEL_PW_PANEL_PASSWORD_CANCEL   4
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+          /* (no menu bars in the resource file) */
+
+
+     /* (no callbacks specified in the resource file) */ 
+
+
+#ifdef __cplusplus
+    }
+#endif
